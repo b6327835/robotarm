154f5fdcbe9464ea8f91b731274bc9c4a181e8c5
b6327835_ubuntu <b6327835@gmail.com>
2024-12-19 18:59:18 +0700

    add more image processing

--- robotarm_gui/color_detection.py
+++ robotarm_gui/color_detection.py
@@ -1,4 +1,3 @@
-
 import cv2
 import numpy as np
 
@@ -22,13 +21,18 @@ class ColorDetector:
         
         elif detect_mode == "black":
             lower_color = np.array([0, 0, 0])
-            upper_color = np.array([180, 255, 50])
+            upper_color = np.array([180, 100, 78])  # Modified S and V max values
             return cv2.inRange(hsv_img, lower_color, upper_color)
         
         return np.zeros_like(hsv_img[:,:,0])
 
     @staticmethod
     def process_mask(color_mask):
-        kernel = np.ones((8,8), np.uint8)
-        color_mask = cv2.morphologyEx(color_mask, cv2.MORPH_OPEN, kernel)
-        return cv2.morphologyEx(color_mask, cv2.MORPH_CLOSE, kernel)
+        kernel = np.ones((2,2), np.uint8)  # Changed kernel size to 2x2
+        # Apply erosion 3 times
+        for _ in range(3):
+            color_mask = cv2.erode(color_mask, kernel)
+        # Apply dilation 3 times
+        for _ in range(3):
+            color_mask = cv2.dilate(color_mask, kernel)
+        return color_mask
